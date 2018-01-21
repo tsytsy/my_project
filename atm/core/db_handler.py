05e42772607ddf01bb5adbd4a77c1b69ebedc946
jie <tsy31415926@163.com>
2018-01-21 16:01:47 +0800

    my project

--- atm/core/db_handler.py
+++
@@ -1,8 +0,0 @@
-# 将DATABASE信息传给conn_parms
-def db_handler(con_parms):
-    if con_parms['engine'] == 'file_storage':
-        return file_db_handler(con_parms)
-
-
-def file_db_handler(con_parms):
-    return con_parms['path']
